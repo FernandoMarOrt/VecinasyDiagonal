928dc92c4fdb36e8cff2c196fd8b63ff91ca19a2
FernandoMarOrt <fernandomartinezortiz003@gmail.com>
2023-01-23 14:37:34 +0100

    Inicio del ejercicio recorrer matriz en diagonal

---
+++ src/main/java/Vecinas/recorrer_en_diagonal.java
@@ -0,0 +1,109 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Vecinas;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author fer
+ */
+public class recorrer_en_diagonal {
+    
+    public static void main(String[] args) {
+        
+        int[][] diagonal = new int[3][3];
+        
+        
+        rellenarAleatorio(diagonal);
+        
+       
+        imprimirMatriz(diagonal);
+        
+        System.out.println("");
+        
+        imprimirMatrizDiagonal(diagonal);
+        
+        
+        //
+        
+        
+        
+        
+    }
+    
+    
+    private static void rellenarAleatorio(int[][] diagonal) {
+        
+        Scanner teclado = new Scanner(System.in);
+        
+        Random numeroAleatorio = new Random();
+        
+        for (int i = 0; i < diagonal.length; i++) {
+            
+            for (int j = 0; j < diagonal[i].length; j++) {
+
+                //Genero un numero aleatorio entre 0 y 20
+                int nAleatorio = numeroAleatorio.nextInt(0, 10 + 1);
+                
+                diagonal[i][j] = nAleatorio;
+                
+            }
+            
+        }
+        
+    }
+    
+    
+    
+    
+     private static void imprimirMatriz(int[][] diagonal) {
+        
+        for (int i = 0; i < diagonal.length; i++) {
+            
+            
+            for (int j = 0; j < diagonal[i].length; j++) {
+                
+                
+                System.out.print(diagonal[i][j] + " | ");
+                
+      
+                
+            }
+            
+            System.out.println("");
+            
+        }
+        
+    }
+     
+     
+     
+     
+     
+     private static void imprimirMatrizDiagonal(int[][] diagonal) {
+        
+        for (int i = 0; i < diagonal.length; i++) {
+            
+            
+            for (int j = 0; j < diagonal[i].length; j++) {
+                
+                
+                System.out.print(diagonal[i][j] + " | ");
+                
+                if(i < diagonal.length) {
+                    i++;
+                }
+                
+            }
+            
+            System.out.println("");
+            
+        }
+        
+    }
+    
+}
